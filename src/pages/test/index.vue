2bdd80518d32b2dbfe54651f18c85516cadc50a7
onlyflowers <1178102633@qq.com>
2020-02-02 20:55:18 +0800

    my first mpvue project

---
+++ src/pages/test/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <!-- 文本格式 Mustach-->
+    <span>学习课程{{className}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      className: 'mpvue 学习'
+    };
+  }
+};
+</script>
+
+<style>
+</style>
